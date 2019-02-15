d6c517f8149a61459691946fa01525e5fbc5e3d5
J M <julianttm@gmail.com>
2019-02-15 11:18:46 -0500

    Adding Folders

---
+++ booksmarklets/hideAndShowPlayer.js
@@ -0,0 +1,54 @@
+function hideAndShowPlayer(){
+   
+    var cssEleNames = [
+        "ytp-title-link yt-uix-sessionlink",
+        "ytp-title-channel-logo",
+        "ytp-watch-later-button ytp-button",
+        "ytp-button ytp-share-button",
+        "ytp-chrome-controls",
+        "ytp-play-progress ytp-swatch-background-color",
+        "ytp-pause-overlay ytp-scroll-min",
+        "ytp-progress-bar",
+        "ytp-chrome-bottom",
+        "ytp-chrome-top ytp-chrome-bottom",
+    ]
+
+    if(document.getElementsByClassName(cssEleNames[0])[0].style.visibility === "hidden"){
+
+        var cssEles = [];
+        for (let ele of cssEleNames){
+            let eleGet = document.getElementsByClassName(ele)[0];
+            if(eleGet){
+                cssEles.push(eleGet);
+            }
+            
+        }
+
+        for(let ele of cssEles){
+           
+            if(ele.style){
+                ele.style.visibility = "";
+            }
+        }
+
+    }else{
+        var cssEles = [];
+        for (let ele of cssEleNames){
+            let eleGet = document.getElementsByClassName(ele)[0];
+            if(eleGet){
+                cssEles.push(eleGet);
+            }
+            
+        }
+
+        for(let ele of cssEles){
+           
+            if( ele.style){
+                ele.style.visibility = "hidden";
+            }
+        }
+    }
+
+  
+}
+hideAndShowPlayer();
