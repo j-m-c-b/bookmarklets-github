8923dba0c5315728a7d931ac16ed7eac55a01052
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2019-11-12 11:59:31 -0500

    Create cleanInsta.js

---
+++ booksmarklets/cleanInsta.js
@@ -0,0 +1,8 @@
+function cleanInsta(){
+    var block1 = document.getElementsByClassName('pbNvD QZZGH  FrS-d'); 
+    block1[0].parentElement.removeChild(block1[0]); 
+    var block2 = document.getElementsByClassName('RnEpo Yx5HN'); 
+    block2[0].parentElement.removeChild(block2[0]); 
+    document.body.style.overflow = 'auto';
+}
+cleanInsta();
